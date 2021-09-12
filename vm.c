75468fe293e9c51d1c16011c52ea9a38ab30b151
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2021-09-12 00:44:10 -0700

    add math opcodes

--- vm.c
+++ vm.c
@@ -31,6 +31,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)     \
+    do {                  \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (false)
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -51,6 +57,16 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:
+                BINARY_OP(+); break;
+            case OP_SUBTRACT:
+                BINARY_OP(-); break;
+            case OP_MULTIPLY:
+                BINARY_OP(*); break;
+            case OP_DIVIDE:
+                BINARY_OP(/); break;
+            case OP_NEGATE:
+                push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
